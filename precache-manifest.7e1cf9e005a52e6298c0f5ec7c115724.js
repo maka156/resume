c080b7c53cb71c1e8bcdb5f6b454996e45fa1d01
Maria Canache <mjcanache@gmail.comgit configexitccolor.ui truegit config --global user.email mjcanache@gmail.com>
2020-03-26 11:59:49 -0300

    Updates

---
+++ precache-manifest.7e1cf9e005a52e6298c0f5ec7c115724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d242a6a52a0529d20f188816ffcc4eca",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "0465de0b11e414408f28",
+    "url": "/resume/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "993df85193bab84993e8",
+    "url": "/resume/static/css/main.d85b6c30.chunk.css"
+  },
+  {
+    "revision": "0465de0b11e414408f28",
+    "url": "/resume/static/js/2.b9b2b788.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/resume/static/js/2.b9b2b788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993df85193bab84993e8",
+    "url": "/resume/static/js/main.b800b7e3.chunk.js"
+  },
+  {
+    "revision": "4c6d88ecadb9dcc80603",
+    "url": "/resume/static/js/runtime-main.1ced625a.js"
+  }
+]);
