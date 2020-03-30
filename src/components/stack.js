c32c984ad0ec3f8865cf3c9d6823308942e07e0e
Maria Canache <mjcanache@gmail.comgit configexitccolor.ui truegit config --global user.email mjcanache@gmail.com>
2020-03-30 12:06:16 -0300

    ADD: media queries

--- src/components/stack.js
+++ src/components/stack.js
@@ -8,11 +8,11 @@ class Stack extends React.Component<props,State> {
       <Card style={{ marginTop: '10px' }}>
         <Card.Body>
           <div className="d-flex flex-row bd-highlight justify-content-around">
-            <div className="p-2 bd-highlight"><img  src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" alt="" width="92" height="92" /></div>
-            <div className="p-2 bd-highlight"><img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png" alt="" width="72" height="72" /></div>
-            <div className="p-2 bd-highlight"><img  src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" /></div>
-            <div className="p-2 bd-highlight"><img  src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg" alt="" width="90" height="90" /></div>
-            <div className="p-2 bd-highlight"><img  src="https://upload.wikimedia.org/wikipedia/commons/5/5c/AWS_Simple_Icons_AWS_Cloud.svg" alt="" width="92" height="92" /></div>
+            <div className="p-2 bd-highlight"><img  src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" alt="" class="hw-custom"/></div>
+            <div className="p-2 bd-highlight"><img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png" alt="" class="hw-custom" /></div>
+            <div className="p-2 bd-highlight"><img  src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" class="hw-custom" /></div>
+            <div className="p-2 bd-highlight"><img  src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg" alt="" class="hw-custom" /></div>
+            <div className="p-2 bd-highlight"><img  src="https://upload.wikimedia.org/wikipedia/commons/5/5c/AWS_Simple_Icons_AWS_Cloud.svg" alt="" class="hw-custom" /></div>
           </div>
         </Card.Body>
       </Card>
