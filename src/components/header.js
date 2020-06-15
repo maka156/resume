12899db51f3a95b245901d463678c32478917214
Maria Canache <mjcanache@@gmail.com>
2020-06-15 16:36:33 -0400

    ADD: fix img url

--- src/components/header.js
+++ src/components/header.js
@@ -10,12 +10,12 @@ class Header extends React.Component<props,State> {
   render() {
     return (
       <Card style={{ width: '100%', marginTop: '20px' }}>
-        <Card.Img variant="top" src="https://media-exp1.licdn.com/dms/image/C5616AQF6lsIXPUr0AQ/profile-displaybackgroundimage-shrink_200_800/0?e=1590019200&v=beta&t=LdEsGO_zGE3dYr-ybSp8HV2EEPuk88NdEyAoVS4cEKo" alt="banner"/>
+        <Card.Img variant="top" src="https://media-exp1.licdn.com/dms/image/C5616AQF6lsIXPUr0AQ/profile-displaybackgroundimage-shrink_200_800/0?e=1597881600&v=beta&t=6dznqDkZynzzqDHFzYL0ESCofCblNbV82dS4gVlk9aU" alt="banner"/>
         <Container>
           <Row>
             <Col>
               <div className="profile-pos">
-                <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFs5WFaBv7B4g/profile-displayphoto-shrink_200_200/0?e=1590019200&amp;v=beta&amp;t=O8pXZSQXZOxJfw1uHNgE1ZCYh-9y0PIMmxoJ7am-AOU" className="rounded-circle profile border" alt="profile"/>
+                <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFs5WFaBv7B4g/profile-displayphoto-shrink_400_400/0?e=1597881600&v=beta&t=Uc4g48smBcvLrohHg_frz6ngZxAaIH9cGDC23SqiWas" className="rounded-circle profile border" alt="profile"/>
               </div>
             </Col>
             <Col>
